5e80750527ac3e723bba11033830b01eeab726c7
Darius J Chuck <darius.j.chuck+github@gmail.com>
2022-12-13 03:27:45 +0100

    improve conversion of strings to values fix string conversion bug for strings that start or end with '

--- fromJsonStr.js
+++ fromJsonStr.js
@@ -11,14 +11,18 @@ const convertKey = key => convertString(key)
 
 const convertString = str => {
   const escaped = escape(str)
-  if (str.trim() !== str) return `'${escaped}'`
+  if (str.trim() !== str || str[0] === "'" || str.at(-1) === "'") {
+    return `'${escaped}'`
+  }
   return escaped
 }
 
 const {opener, closer} = defaultDelimiters
 const convertValue = value => {
   if (typeof value === 'string') {
-    return stringToHeredoc(value)//`[${convertString(value)}]`
+    const str = convertString(value)
+    if (str.length > value.length) return stringToHeredoc(value)
+    return opener + str + closer
   }
 
   return opener + value + closer
