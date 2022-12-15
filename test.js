04a84fbe369173eea205b46aa2f64a183e98877e
Darius J Chuck <darius.j.chuck+github@gmail.com>
2022-12-15 04:22:55 +0100

    all console logs are now tests

--- test.js
+++ test.js
@@ -1,69 +1,130 @@
 import {fromString} from './mod.js'
 import { assertEquals } from "https://deno.land/std@0.165.0/testing/asserts.ts";
 
-const ret = fromString(`
-This is a comment
-
-title [jevkoconfig1 Example]
-
-owner [
-  name [tester]
-  dob \`''2020-08-05T20:30:01+09:00[Asia/Tokyo][u-ca=japanese]''
-]
-
-database [
-  enabled [true]
-  quoted ['true]
-  ports [
-    [8000]
-    [8001]
-    [8002]
+Deno.test('readme example', () => {
+  const ret = fromString(`
+  This is a comment
+  
+  title [jevkoconfig1 Example]
+  
+  owner [
+    name [tester]
+    dob \`''2020-08-05T20:30:01+09:00[Asia/Tokyo][u-ca=japanese]''
   ]
-  data [ [[delta] [phi]] [3.14] ]
-  temp targets [ cpu [79.5] case [72.0] ]
-]
-
-servers [
-  alpha [
-    ip [10.0.0.1]
-    role [frontend]
+  
+  database [
+    enabled [true]
+    quoted ['true]
+    ports [
+      [8000]
+      [8001]
+      [8002]
+    ]
+    data [ [[delta] [phi]] [3.14] ]
+    temp targets [ cpu [79.5] case [72.0] ]
   ]
-  beta [
-    ip [10.0.0.2]
-    role [backend]
+  
+  servers [
+    alpha [
+      ip [10.0.0.1]
+      role [frontend]
+    ]
+    beta [
+      ip [10.0.0.2]
+      role [backend]
+    ]
+  
+    -discarded key [[with][a][value]]
+    -[discarded section]
   ]
-
-  -discarded key [[with][a][value]]
-  -[discarded section]
-]
-
-embedded documents [
-  some json \`'json'
-  { 
-    "id": "b3df0d",
-    "count": 55,
-    "props": {
-      "return code": "59503a7b",
-      "status": "pending"
-    },
-    "associated ids": [
-      "3adf7c",
-      "ff0df7",
-      "3aa670"
+  
+  embedded documents [
+    some json \`'json'
+    { 
+      "id": "b3df0d",
+      "count": 55,
+      "props": {
+        "return code": "59503a7b",
+        "status": "pending"
+      },
+      "associated ids": [
+        "3adf7c",
+        "ff0df7",
+        "3aa670"
+      ],
+      "parent": null 
+    }
+    'json'
+    more json \`'json'55'json'
+    json string \`'json'"\\n\\tsomething\\u0000"'json'
+    json array \`'json'[1, 2, 3, 4, null]'json'
+  ]
+  `)
+  
+  assertEquals(JSON.stringify(ret, null, 2), String.raw`{
+  "title": "jevkoconfig1 Example",
+  "owner": {
+    "name": "tester",
+    "dob": "2020-08-05T20:30:01+09:00[Asia/Tokyo][u-ca=japanese]"
+  },
+  "database": {
+    "enabled": true,
+    "quoted": "true",
+    "ports": [
+      8000,
+      8001,
+      8002
     ],
-    "parent": null 
+    "data": [
+      [
+        "delta",
+        "phi"
+      ],
+      3.14
+    ],
+    "temp targets": {
+      "cpu": 79.5,
+      "case": 72
+    }
+  },
+  "servers": {
+    "alpha": {
+      "ip": "10.0.0.1",
+      "role": "frontend"
+    },
+    "beta": {
+      "ip": "10.0.0.2",
+      "role": "backend"
+    }
+  },
+  "embedded documents": {
+    "some json": {
+      "id": "b3df0d",
+      "count": 55,
+      "props": {
+        "return code": "59503a7b",
+        "status": "pending"
+      },
+      "associated ids": [
+        "3adf7c",
+        "ff0df7",
+        "3aa670"
+      ],
+      "parent": null
+    },
+    "more json": 55,
+    "json string": "\n\tsomething\u0000",
+    "json array": [
+      1,
+      2,
+      3,
+      4,
+      null
+    ]
   }
-  'json'
-  more json \`'json'55'json'
-  json string \`'json'"\\n\\tsomething\\u0000"'json'
-  json array \`'json'[1, 2, 3, 4, null]'json'
-]
-`)
-
-console.log(JSON.stringify(ret, null, 2))
-console.log(ret.database.test)
-
-console.log("TODO: turn the above console logs into proper tests")
+}`)
+  assertEquals(ret.database.test, undefined)
+})
 
 Deno.test('array with comments', () => {
   const obj = fromString(`array [
